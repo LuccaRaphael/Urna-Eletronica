52fe97b5a14051f141f42a1ca82cf88efd092fec
charlenefialho <fialhocharlene@gmail.com>
2023-02-14 18:55:30 -0300

    finalizando

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';/* preenche dessa maneira -> let numero = '78111'*/
+let votoBranco = false;
+let votos = [];
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
 
+    votoBranco = false;
     let numeroHTML = '';
+    numero = '';/*zerar o numero quando for começar etapa denv*/
 
     /*for para exibir a  quantidade de quadrados de cada etapa*/ 
     for(let i =0;i< etapa.numeros; i++)
@@ -35,25 +39,33 @@ function comecarEtapa(){
 function atualizaInterface(){
     let etapa = etapas[etapaAtual];
     let candidato = etapa.candidatos.filter((item)=>{
-        if(item.numero === numero){
+        if (item.numero === numero){
             return true;
-        }else{
+        } else {
             return false;
         }  
     });
-    if(candidato.length > 0)
-        {
-            candidato = candidato[0];
-            seuVotoPara.style.display = 'block';
-            aviso.style.display = 'block';
-            descricao.innerHTML = `Nome:${candidato.nome}<br/>Partido:${candidato.partido}<br/>`;
-            let fotosHTML = '';
-            for(let i in candidato.fotos)
+    if(candidato.length > 0){
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome:${candidato.nome}<br/>Partido:${candidato.partido}<br\>`;
+        let fotosHTML = '';
+        for(let i in candidato.fotos)
             {
-                fotosHTML += `<div class="d-1-image"> <img src="img/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`
+                if(candidato.fotos[i].small){
+                    fotosHTML += `<div class="d-1-image small"> <img src="img/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`
+                }else{
+                    fotosHTML += `<div class="d-1-image"> <img src="img/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+                }
             }
             lateral.innerHTML = fotosHTML;
-        }
+        
+    }else{
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
 
 }
 
@@ -77,14 +89,50 @@ function clicou(n){
 }
 
 function branco(){
-    alert("Clicou em branco");
+        numero = '';
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+        lateral.innerHTML = '';
+
 }
 
 function corrige(){
-    alert("Clicou em corrige");
+    comecarEtapa();
 }
 function confirma(){
-    alert("Clicou em confirma");
+    let etapa = etapas[etapaAtual];
+
+    let votoConfirmado = false;
+
+    if(votoBranco === true)
+    {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+    }else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+
+    if(votoConfirmado)
+    {
+        etapaAtual++;
+        if(etapas[etapaAtual] != undefined)
+        {
+            comecarEtapa();
+        } else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM!!</div>';
+            console.log(votos);
+        }
+    }
 }
 
 comecarEtapa();
